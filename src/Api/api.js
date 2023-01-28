b02d2f50c3e4a194d824ce218e64159e72f0951d
ShofiqulShuvo <logic.shuvo@gmail.com>
2023-01-28 07:00:42 +0600

    fixing url

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,6 +1,6 @@
 
 
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL;
+export const BASE_URL = `${process.env.REACT_APP_BACKEND_URL}` ;
 
 export const postConfigure = (data) => {
   return {
