428053d0ef88d61c6fcadfe101998288d2b411e7
ShofiqulShuvo <logic.shuvo@gmail.com>
2023-01-28 07:07:26 +0600

    fixing url

--- src/Routes/Index.js
+++ src/Routes/Index.js
@@ -1,24 +1,17 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { lazy } from 'react'
-import { Suspense } from 'react'
-import Spiner from '../Components/Spiner'
 
-// import FrontendRoutes from './Frontend/FrontendRoutes'
-// import AdminRoutes from './Admin/AdminRoutes'
+import FrontendRoutes from './Frontend/FrontendRoutes'
+import AdminRoutes from './Admin/AdminRoutes'
 
-const FrontendRoutes = lazy(()=> import('./Frontend/FrontendRoutes'))
-const AdminRoutes = lazy(()=> import('./Admin/AdminRoutes'))
 
 const Index = () => {
   return (
     <>
-      <Suspense fallback={<div className='text-center mt-4 h3'><Spiner /></div>}>
         <Routes>
             <Route path='/*' element={<FrontendRoutes />} />
             <Route path='/admin/*' element={<AdminRoutes />} />
         </Routes>
-      </Suspense>
     </>
   )
 }
